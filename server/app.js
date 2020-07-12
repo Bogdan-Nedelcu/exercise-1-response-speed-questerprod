e1dc38e3de8c3a1afbdd4747fb08abab1adaaa05
Bogdan Nedelcu <bogdanned32@gmail.com>
2020-07-12 15:26:44 +0000

    I am finished

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const responseTime = require("response-time");
 const expensiveTask = require("./utils/expensiveTask");
 
-const { startWorker, tasksEndpoint } = require("./utils/taskRunner");
+const { addTask, startWorker, tasksEndpoint } = require("./utils/taskRunner");
 const e = require("express");
 
 const app = express();
@@ -13,8 +13,9 @@ app.use(responseTime());
 app.get("/", function (req, res) {
   const n = parseInt(req.query.n);
   // heavy server work here
-  const result = expensiveTask(n);
-  res.status(200).json({ result: "done" });
+  // const result = expensiveTask(n);
+  addTask(expensiveTask, n)
+  res.status(200).json({ status: "task-started" });
 });
 
 module.exports = app;
